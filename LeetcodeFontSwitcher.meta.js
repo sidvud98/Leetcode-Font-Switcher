57d69d2c9c8fb3a84f804f77cce49e83fffb6f0d
sidvud98 <sidvud98@gmail.com>
2024-01-28 08:09:53 +0530

    updated links

--- LeetcodeFontSwitcher.meta.js
+++ LeetcodeFontSwitcher.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Leetcode Editor Font Modifier
-// @namespace    none
+// @namespace    https://github.com/sidvud98/Leetcode-Font-Switcher
 // @version      1
 // @description  Sets Leetcode Editor Font as Cascadia Code
 // @author       Siddharth VS
-// @updateURL    none
-// @downloadURL  none
+// @updateURL    https://raw.githubusercontent.com/YukiDayDreamer/LeetCode-Timer/master/LeetCode-Timer.meta.js
+// @downloadURL  https://raw.githubusercontent.com/YukiDayDreamer/LeetCode-Timer/master/LeetCode-Timer.user.js
 // @match        https://leetcode.com/problems/*
 // @grant        none
 // ==/UserScript==
--- LeetcodeFontSwitcher.user.js
+++ LeetcodeFontSwitcher.user.js
@@ -1,23 +1,22 @@
 // ==UserScript==
 // @name         Leetcode Editor Font Modifier
-// @namespace    none
+// @namespace    https://github.com/sidvud98/Leetcode-Font-Switcher
 // @version      1
 // @description  Sets Leetcode Editor Font as Cascadia Code
 // @author       Siddharth VS
-// @updateURL    none
-// @downloadURL  none
+// @updateURL    https://raw.githubusercontent.com/YukiDayDreamer/LeetCode-Timer/master/LeetCode-Timer.meta.js
+// @downloadURL  https://raw.githubusercontent.com/YukiDayDreamer/LeetCode-Timer/master/LeetCode-Timer.user.js
 // @match        https://leetcode.com/problems/*
 // @grant        none
 // ==/UserScript==
 
 
 
-
-(function() {
+(function () {
     setTimeout(() => {
         var elem = document.querySelector(".view-lines.monaco-mouse-cursor-text");
-        if(elem){
+        if (elem) {
             elem.style.fontFamily = "Cascadia code"
         };
-    },2000)
+    }, 2000)
 })();
